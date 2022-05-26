ceabda1423ac3a079a22880f9b1854ad1d631400
yatharth <lakhatey@gmail.com>
2022-05-26 19:48:11 +0530

    Home pg -> saving text input using state

--- google-sclone-v1/src/components/Search.jsx
+++ google-sclone-v1/src/components/Search.jsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import MicIcon from "@mui/icons-material/Mic";
 import Button from "@mui/material/Button";
 import "./Search.css";
 function Search() {
+  // this state is keeping track of text inside input
+  const [input, setInput] = useState("");
+
+  // this is doing search on btn click
+  const search = (e) => {
+    e.preventDefault();
+    // this prevent def --> prevent running submit when page relods on click
+
+    console.log("hit the submit ");
+  };
+  console.log(input);
   return (
     <div className="search">
       <div className="search_inp">
         <SearchIcon className="search_inpIcon" />
-        <input type="text" />
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
         <MicIcon className="search_inp_mic" />
       </div>
 
       <div className="search__btns">
-        <Button variant="outlined">Google Search</Button>
+        <Button variant="outlined" onClick={search}>
+          Google Search
+        </Button>
         <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
     </div>
